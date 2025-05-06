1d9fdbc694ec7009c56373caf3fab26632b92e06
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-05-06 17:16:38 -0300

    FEAT: adiciona lógica dos handlers ao projeto

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/joaomarcosg/Projeto-Habit-Manager-Golang/internal/habit"
 	"github.com/joaomarcosg/Projeto-Habit-Manager-Golang/internal/store/mysqlstore"
 )
 
@@ -20,11 +21,14 @@ func run() error {
 		log.Printf("Failed to connect: %v", err)
 	}
 
+	handler := habit.NewHandler(sqlDB)
+
 	s := http.Server{
 		ReadTimeout:  10 * time.Second,
 		IdleTimeout:  time.Minute,
 		WriteTimeout: 10 * time.Second,
 		Addr:         ":8080",
+		Handler:      handler,
 	}
 
 	if err := s.ListenAndServe(); err != nil {
---
+++ internal/habit/handler.go
@@ -0,0 +1,20 @@
+package habit
+
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func NewHandler(db *sql.DB) http.Handler {
+
+	r := chi.NewMux()
+
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+
+	return r
+}
