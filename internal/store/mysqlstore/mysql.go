42f11f18ee98830498d26d357929e6a3b2c8197a
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-05-06 16:49:23 -0300

    FEAT: adiciona lógica de conexão com BD

--- internal/store/mysqlstore/mysql.go
+++ internal/store/mysqlstore/mysql.go
@@ -1,1 +1,19 @@
 package mysqlstore
+
+import (
+	"database/sql"
+	"log"
+)
+
+func ConnectDB(datasource string) *sql.DB {
+
+	db, err := sql.Open("mysql", datasource)
+	if err != nil {
+		log.Printf("Error connect to database: %v", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		log.Printf("Failed to ping MySQL: %v", err)
+	}
+
+}
