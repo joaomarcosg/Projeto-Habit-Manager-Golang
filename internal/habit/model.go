cbdd36c9e8fc6e158edfb184fe4e613fb632b18e
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-05-13 16:13:45 -0300

    FEAT: adiciona interface HabitRepository para uso no service.go

--- internal/habit/model.go
+++ internal/habit/model.go
@@ -1,9 +1,16 @@
 package habit
 
-import "time"
+import (
+	"context"
+	"time"
+)
 
 type Habit struct {
 	ID        int64     `json:"id"`
 	Name      string    `json:"name"`
 	CreatedAt time.Time `json:"created_at"`
+}
+
+type HabitRepository interface {
+	CreateHabit(ctx context.Context, name string) error
 }
