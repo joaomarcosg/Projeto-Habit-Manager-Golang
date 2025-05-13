1c2f39a59bd578953c91a043ca66712bd86e5774
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-05-13 16:18:45 -0300

    FEAT: adiciona injenção no service.go

--- internal/habit/service.go
+++ internal/habit/service.go
@@ -1,1 +1,15 @@
 package habit
+
+import "context"
+
+type Service struct {
+	repo HabitRepository
+}
+
+func NewService(repo HabitRepository) *Service {
+	return &Service{repo: repo}
+}
+
+func (s *Service) CreateHabit(ctx context.Context, name string) error {
+	return s.repo.CreateHabit(ctx, name)
+}
