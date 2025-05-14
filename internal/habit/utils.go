7480ebd763ee766ba3372f120938f3da01957444
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-05-14 15:54:22 -0300

    FEAT: adiciona função para simplificar JSON

--- internal/habit/utils.go
+++ internal/habit/utils.go
@@ -1,7 +1,31 @@
 package habit
 
-import "context"
+import (
+	"context"
+	"encoding/json"
+	"log/slog"
+	"net/http"
+)
 
 type HabitRepository interface {
 	CreateHabit(ctx context.Context, name string) error
+}
+
+func sendJSON(w http.ResponseWriter, resp apiResponse, status int) {
+
+	data, err := json.Marshal(resp)
+	if err != nil {
+		slog.Error("failed to marshal json data", "error", err, "response", resp)
+		sendJSON(w, apiResponse{Error: "something went wrong"}, http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	w.WriteHeader(status)
+	if _, err := w.Write(data); err != nil {
+		slog.Error("failed to write response to client", "error", err)
+		return
+	}
+
 }
