786a91aaf88473e540d0b4bf0aa7edde18b03881
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-05-14 16:08:02 -0300

    FEAT: conclui funcionalidade da rota POST "/habits"

--- internal/habit/create_handler.go
+++ internal/habit/create_handler.go
@@ -1,9 +1,29 @@
 package habit
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type inputHabit struct {
+	Name string `json:"name"`
+}
 
 func handleCreateHabit(svc *Service) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
 
+	return func(w http.ResponseWriter, r *http.Request) {
+		var input inputHabit
+
+		if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
+			sendJSON(w, apiResponse{Error: "invalid body"}, http.StatusUnprocessableEntity)
+			return
+		}
+
+		if err := svc.CreateHabit(r.Context(), input.Name); err != nil {
+			sendJSON(w, apiResponse{Error: "could not create habit"}, http.StatusInternalServerError)
+			return
+		}
+
+		sendJSON(w, apiResponse{Data: input}, http.StatusCreated)
 	}
 }
