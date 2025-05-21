8aaeca9dc780bea0ad214b8b206917bd645bd1ac
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-05-21 16:21:08 -0300

    FIX: corrige migration

--- internal/store/mysqlstore/migrations/000002_modify_habits.down.sql
+++ internal/store/mysqlstore/migrations/000002_modify_habits.down.sql
@@ -1,1 +1,9 @@
-ALTER TABLE habits DROP COLUMN category, description, frequency, start_date, target_date, priority;
+ALTER TABLE habits
+    DROP COLUMN category,
+    DROP COLUMN description,
+    DROP COLUMN frequency,
+    DROP COLUMN start_date,
+    DROP COLUMN target_date,
+    DROP COLUMN priority,
+    DROP COLUMN updated_at,
+    MODIFY COLUMN name VARCHAR(255) NOT NULL;
