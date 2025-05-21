63f671b20db3b474ba5ed281bb00d3e2d371b3c8
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-05-21 16:40:07 -0300

    FIX: corrige migration

--- internal/store/mysqlstore/migrations/000001_habits.up.sql
+++ internal/store/mysqlstore/migrations/000001_habits.up.sql
@@ -7,7 +7,7 @@ CREATE TABLE habits (
         'sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'
     ),
     start_date DATE NOT NULL DEFAULT '2025-01-01',
-    target_date DATE NOT NULL '2025-01-01',
+    target_date DATE NOT NULL DEFAULT '2025-01-01',
     priority TINYINT UNSIGNED NOT NULL DEFAULT 1 CHECK ((priority BETWEEN 1 AND 10)),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
