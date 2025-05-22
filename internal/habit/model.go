8a0d80f21f590734526979e44f8c388cd9062180
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-05-22 16:53:56 -0300

    FIX: corrige struct Habit

--- internal/habit/model.go
+++ internal/habit/model.go
@@ -5,7 +5,14 @@ import (
 )
 
 type Habit struct {
-	ID        int64     `json:"id"`
-	Name      string    `json:"name"`
-	CreatedAt time.Time `json:"created_at"`
+	ID          int64     `json:"id"`
+	Name        string    `json:"name"`
+	Category    string    `json:"category"`
+	Description string    `json:"description"`
+	Frequency   string    `json:"frequency"`
+	StartDate   time.Time `json:"start_date"`
+	TargetDate  time.Time `json:"target_date"`
+	Priority    uint8     `json:"priority"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
