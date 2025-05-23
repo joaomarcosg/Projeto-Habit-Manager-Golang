dbf2d5e9d5cbbc7b3f73c97e62ebc92b6c274f43
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-05-23 15:45:40 -0300

    FIX: corrige método CreateHabit

--- internal/store/mysqlstore/repository.go
+++ internal/store/mysqlstore/repository.go
@@ -1,9 +1,41 @@
 package mysqlstore
 
+import (
+	"context"
+
+	"github.com/joaomarcosg/Projeto-Habit-Manager-Golang/internal/entity"
+)
+
 type HabitRepository struct {
 	q *Queries
 }
 
 func NewHabitRepository(q *Queries) *HabitRepository {
 	return &HabitRepository{q: q}
+}
+
+func (r *HabitRepository) CreateHabit(ctx context.Context, habit entity.Habit) (int64, error) {
+	result, err := r.q.CreateHabit(ctx, CreateHabitParams{
+		Name:        habit.Name,
+		Category:    habit.Category,
+		Description: habit.Description,
+		Frequency: NullHabitsFrequency{
+			HabitsFrequency: HabitsFrequency(habit.Frequency),
+			Valid:           true,
+		},
+		StartDate:  habit.StartDate,
+		TargetDate: habit.TargetDate,
+		Priority:   habit.Priority,
+	})
+
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return id, nil
 }
