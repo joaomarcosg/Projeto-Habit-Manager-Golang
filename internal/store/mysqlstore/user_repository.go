b3edf977dc6b91da2021c34c4aefd41d30381ede
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-05-31 14:32:07 -0300

    FEAT: adiciona método CreateUser no arquivo user_repository.go

--- internal/store/mysqlstore/user_repository.go
+++ internal/store/mysqlstore/user_repository.go
@@ -1,9 +1,35 @@
 package mysqlstore
 
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/joaomarcosg/Projeto-Habit-Manager-Golang/internal/entity"
+)
+
 type UserRepository struct {
 	q *Queries
 }
 
 func NewUserRepository(q *Queries) *UserRepository {
 	return &UserRepository{q: q}
+}
+
+func (r *UserRepository) CreateUser(ctx context.Context, user entity.User) (entity.User, error) {
+
+	user.ID = uuid.New().String()
+
+	_, err := r.q.CreateUser(ctx, CreateUserParams{
+		ID:       user.ID,
+		Name:     user.Name,
+		Email:    user.Email,
+		Password: user.Password,
+	})
+
+	if err != nil {
+		return entity.User{}, err
+	}
+
+	return user, nil
+
 }
