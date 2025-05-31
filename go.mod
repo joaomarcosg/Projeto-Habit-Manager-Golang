3eb7b2c8d690cb903c0a9b46b4498008b6441934
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-05-31 15:11:17 -0300

    FEAT: adiciona método CreateUser em user_service.go e também atualiza pacotes

--- go.mod
+++ go.mod
@@ -5,9 +5,8 @@ go 1.24.2
 require (
 	github.com/go-chi/chi/v5 v5.2.1
 	github.com/go-sql-driver/mysql v1.9.2
+	github.com/google/uuid v1.6.0
+	golang.org/x/crypto v0.38.0
 )
 
-require (
-	filippo.io/edwards25519 v1.1.0 // indirect
-	github.com/google/uuid v1.6.0 // indirect
-)
+require filippo.io/edwards25519 v1.1.0 // indirect
---
+++ internal/habit/user_service.go
@@ -0,0 +1,37 @@
+package habit
+
+import (
+	"context"
+
+	"github.com/joaomarcosg/Projeto-Habit-Manager-Golang/internal/entity"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type UserService struct {
+	repo UserRepository
+}
+
+func NewUserService(repo UserRepository) *UserService {
+	return &UserService{repo: repo}
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	return string(bytes), err
+}
+
+func (s *UserService) CreateUser(ctx context.Context, name, email, password string) (entity.User, error) {
+
+	hash, err := HashPassword(password)
+	if err != nil {
+		return entity.User{}, err
+	}
+
+	user := entity.User{
+		Name:     name,
+		Email:    email,
+		Password: hash,
+	}
+
+	return s.repo.CreateUser(ctx, user)
+}
