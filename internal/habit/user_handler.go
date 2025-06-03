86ed063a4fc95f713b34eba7085022c4a6055e09
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-06-03 17:03:51 -0300

    FEAT: adiciona handler handleSignupUser

--- internal/habit/user_handler.go
+++ internal/habit/user_handler.go
@@ -1,1 +1,26 @@
 package habit
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type CreateUserReq struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func handleSignupUser(svc *Service) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		var user CreateUserReq
+
+		if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+			sendJSON(w, apiResponse{Error: "invalid body"}, http.StatusUnprocessableEntity)
+			return
+		}
+
+	}
+
+}
