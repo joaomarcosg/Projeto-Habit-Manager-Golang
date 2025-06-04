5094eb2a7a5005cd44ade392aef057fba40adb42
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-06-04 17:00:03 -0300

    FXI: corrige import de user_handler.go

--- internal/api/user_handler.go
+++ internal/api/user_handler.go
@@ -4,6 +4,9 @@ import (
 	"encoding/json"
 	"log/slog"
 	"net/http"
+
+	"github.com/joaomarcosg/Projeto-Habit-Manager-Golang/internal/service"
+	"github.com/joaomarcosg/Projeto-Habit-Manager-Golang/internal/utils"
 )
 
 type CreateUserReq struct {
@@ -12,24 +15,24 @@ type CreateUserReq struct {
 	Password string `json:"password"`
 }
 
-func handleSignupUser(svc *UserService) http.HandlerFunc {
+func handleSignupUser(svc *service.UserService) http.HandlerFunc {
 
 	return func(w http.ResponseWriter, r *http.Request) {
 		var user CreateUserReq
 
 		if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
-			sendJSON(w, apiResponse{Error: "invalid body"}, http.StatusUnprocessableEntity)
+			utils.SendJSON(w, utils.ApiResponse{Error: "invalid body"}, http.StatusUnprocessableEntity)
 			return
 		}
 
 		id, err := svc.CreateUser(r.Context(), user.Name, user.Email, user.Password)
 		if err != nil {
 			slog.Error("failed to create user", "error", err)
-			sendJSON(w, apiResponse{Error: "could not create user"}, http.StatusInternalServerError)
+			utils.SendJSON(w, utils.ApiResponse{Error: "could not create user"}, http.StatusInternalServerError)
 			return
 		}
 
-		sendJSON(w, apiResponse{Data: id.ID}, http.StatusCreated)
+		utils.SendJSON(w, utils.ApiResponse{Data: id.ID}, http.StatusCreated)
 
 	}
 
