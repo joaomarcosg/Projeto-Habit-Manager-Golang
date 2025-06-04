5b183b4eadcb01f76a9fde2ebd67f500f49e43ac
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-06-04 16:42:54 -0300

    CHORE: criar pacote utils

---
+++ internal/utils/utils.go
@@ -0,0 +1,39 @@
+package utils
+
+import (
+	"context"
+	"encoding/json"
+	"log/slog"
+	"net/http"
+
+	"github.com/joaomarcosg/Projeto-Habit-Manager-Golang/internal/entity"
+)
+
+type HabitRepository interface {
+	CreateHabit(ctx context.Context, habit entity.Habit) (int64, error)
+	ListHabits(ctx context.Context) ([]entity.Habit, error)
+	DeleteHabit(ctx context.Context, id int64) (bool, error)
+}
+
+type UserRepository interface {
+	CreateUser(ctx context.Context, user entity.User) (entity.User, error)
+}
+
+func sendJSON(w http.ResponseWriter, resp apiResponse, status int) {
+
+	data, err := json.Marshal(resp)
+	if err != nil {
+		slog.Error("failed to marshal json data", "error", err, "response", resp)
+		sendJSON(w, apiResponse{Error: "something went wrong"}, http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	w.WriteHeader(status)
+	if _, err := w.Write(data); err != nil {
+		slog.Error("failed to write response to client", "error", err)
+		return
+	}
+
+}
