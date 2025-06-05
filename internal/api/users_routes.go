db17c0d741c67727b8c870c8495d93812221d4b4
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-06-05 16:16:25 -0300

    FEAT: adiciona rota POST para o handler handleSignupUser

---
+++ internal/api/users_routes.go
@@ -0,0 +1,23 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joaomarcosg/Projeto-Habit-Manager-Golang/internal/services"
+)
+
+func NewUserHandler(svc *services.UserService) http.Handler {
+
+	r := chi.NewMux()
+
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+
+	r.Post("/users/signup", handleSignupUser(svc))
+
+	return r
+
+}
