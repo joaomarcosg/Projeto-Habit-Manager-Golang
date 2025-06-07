429c02b0f07116fffbbdc87bd00661e8ddf97cab
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-06-07 11:50:45 -0300

    FEAT: adiciona funções para tratamento do json

--- internal/utils/json_utils.go
+++ internal/utils/json_utils.go
@@ -1,1 +1,36 @@
 package utils
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/joaomarcosg/Projeto-Habit-Manager-Golang/internal/validator"
+)
+
+func EncodeJson[T any](w http.ResponseWriter, r *http.Request, statusCode int, data T) error {
+
+	w.Header().Set("Content-Type", "Application/json")
+	w.WriteHeader(statusCode)
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		return fmt.Errorf("failed to enconde json %w", err)
+	}
+
+	return nil
+
+}
+
+func DecodeJson[T validator.Validator](r *http.Request) (T, map[string]string, error) {
+
+	var data T
+	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+		return data, nil, fmt.Errorf("failed to decode json %w", err)
+	}
+
+	if problems := data.Valid(r.Context()); len(problems) > 0 {
+		return data, problems, fmt.Errorf("invalid %T: %d problems", data, len(problems))
+	}
+
+	return data, nil, nil
+
+}
