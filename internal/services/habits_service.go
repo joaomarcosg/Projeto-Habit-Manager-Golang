db30c2ee21e5ddd73701306640ba803605426c2c
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-07-03 20:44:16 -0300

    FIX: corrige métodos CreateHabit e DeleteHabit para receber userID

--- internal/services/habits_service.go
+++ internal/services/habits_service.go
@@ -15,14 +15,14 @@ func NewHabitService(repo utils.HabitRepository) *HabitService {
 	return &HabitService{repo: repo}
 }
 
-func (s *HabitService) CreateHabit(ctx context.Context, habit entity.Habit) (int64, error) {
-	return s.repo.CreateHabit(ctx, habit)
+func (s *HabitService) CreateHabit(ctx context.Context, userID string, habit entity.Habit) (int64, error) {
+	return s.repo.CreateHabit(ctx, userID, habit)
 }
 
 func (s *HabitService) ListHabits(ctx context.Context, userID string) ([]entity.Habit, error) {
 	return s.repo.ListHabits(ctx, userID)
 }
 
-func (s *HabitService) DeleteHabit(ctx context.Context, id int64) (bool, error) {
-	return s.repo.DeleteHabit(ctx, id)
+func (s *HabitService) DeleteHabit(ctx context.Context, userID string, id int64) (bool, error) {
+	return s.repo.DeleteHabit(ctx, userID, id)
 }
