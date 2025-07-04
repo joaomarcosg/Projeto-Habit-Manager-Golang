e06921862e5e591e0a9a7d72e3f14a71d182d560
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-07-04 16:08:48 -0300

    FIX: corrige tratatamento do JSON do handler CreateHabit

--- internal/api/habit_handlers.go
+++ internal/api/habit_handlers.go
@@ -1,62 +1,59 @@
 package api
 
 import (
-	"encoding/json"
 	"log/slog"
 	"net/http"
 	"strconv"
-	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/joaomarcosg/Projeto-Habit-Manager-Golang/internal/entity"
 	"github.com/joaomarcosg/Projeto-Habit-Manager-Golang/internal/services"
+	"github.com/joaomarcosg/Projeto-Habit-Manager-Golang/internal/usecase/habit"
 	"github.com/joaomarcosg/Projeto-Habit-Manager-Golang/internal/utils"
 )
-
-type inputHabit struct {
-	Name        string    `json:"name"`
-	Category    string    `json:"category"`
-	Description string    `json:"description"`
-	Frequency   string    `json:"frequency"`
-	StartDate   time.Time `json:"start_date"`
-	TargetDate  time.Time `json:"target_date"`
-	Priority    uint8     `json:"priority"`
-}
 
 func handleCreateHabit(svc *services.HabitService) http.HandlerFunc {
 
 	return func(w http.ResponseWriter, r *http.Request) {
-		var input inputHabit
 
 		userID, ok := r.Context().Value(userIDKey).(string)
 		if !ok || userID == "" {
-			utils.SendJSON(w, utils.ApiResponse{Error: "unauthorized"}, http.StatusUnauthorized)
+			utils.EncodeJson(w, r, http.StatusUnauthorized, map[string]any{
+				"error": "unauthorized",
+			})
 			return
 		}
 
-		if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
-			utils.SendJSON(w, utils.ApiResponse{Error: "invalid body"}, http.StatusUnprocessableEntity)
+		data, problems, err := utils.DecodeValidJson[habit.CreateHabitReq](r)
+		if err != nil {
+			_ = utils.EncodeJson(w, r, http.StatusUnprocessableEntity, problems)
 			return
 		}
 
 		habit := entity.Habit{
-			Name:        input.Name,
-			Category:    input.Category,
-			Description: input.Description,
-			Frequency:   input.Frequency,
-			StartDate:   input.StartDate,
-			TargetDate:  input.TargetDate,
-			Priority:    input.Priority,
+			Name:        data.Name,
+			Category:    data.Category,
+			Description: data.Description,
+			Frequency:   data.Frequency,
+			StartDate:   data.StartDate,
+			TargetDate:  data.TargetDate,
+			Priority:    data.Priority,
 		}
 
 		var id int64
 
-		id, err := svc.CreateHabit(r.Context(), userID, habit)
+		id, err = svc.CreateHabit(r.Context(), userID, habit)
 		if err != nil {
 			slog.Error("failed to create habit", "error", err)
-			utils.SendJSON(w, utils.ApiResponse{Error: "could not create habit"}, http.StatusInternalServerError)
+			utils.EncodeJson(w, r, http.StatusInternalServerError, map[string]any{
+				"error": "could not create habit",
+			})
 			return
 		}
+
+		utils.EncodeJson(w, r, http.StatusCreated, map[string]any{
+			"habit_id": id,
+		})
 
 		utils.SendJSON(w, utils.ApiResponse{ID: id}, http.StatusCreated)
 	}
