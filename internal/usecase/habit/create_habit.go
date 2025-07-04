2c360648337d11d9f667052890a15c0ecb8f2c27
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-07-03 22:53:47 -0300

    FEAT: adiciona  função Valid para validar o json da requisição de criar hábitos

---
+++ internal/usecase/habit/create_habit.go
@@ -0,0 +1,33 @@
+package habit
+
+import (
+	"context"
+	"time"
+
+	"github.com/joaomarcosg/Projeto-Habit-Manager-Golang/internal/validator"
+)
+
+type CreateHabitReq struct {
+	Name        string    `json:"name"`
+	Category    string    `json:"category"`
+	Description string    `json:"description"`
+	Frequency   string    `json:"frequency"`
+	StartDate   time.Time `json:"start_date"`
+	TargetDate  time.Time `json:"target_date"`
+	Priority    uint8     `json:"priority"`
+}
+
+func (req CreateHabitReq) Valid(ctx context.Context) validator.Evaluator {
+
+	var eval validator.Evaluator
+
+	eval.CheckField(validator.NotBlank(req.Name), "name", "this field cannot be empty")
+	eval.CheckField(validator.NotBlank(req.Category), "category", "this field cannot be empty")
+	eval.CheckField(validator.NotBlank(req.Description), "description", "this field cannot be empty")
+	eval.CheckField(
+		validator.MinChars(req.Description, 10) &&
+			validator.MaxChars(req.Description, 255), "description", "this field must have a length between 10 and 255")
+	eval.CheckField(validator.NotBlank(req.Frequency), "frequency", "this field cannot be empty")
+
+	return eval
+}
