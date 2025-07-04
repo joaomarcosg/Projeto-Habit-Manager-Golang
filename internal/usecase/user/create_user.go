248e80b3e4c0d04e25fbce7f530cb28648e1c517
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-07-03 22:36:30 -0300

    CHORE: corrige erro de digitação

--- internal/usecase/user/create_user.go
+++ internal/usecase/user/create_user.go
@@ -16,8 +16,8 @@ func (req CreateUserReq) Valid(ctx context.Context) validator.Evaluator {
 
 	var eval validator.Evaluator
 
-	eval.CheckField(validator.NotBlank(req.Name), "name", "this filed cannot be empty")
-	eval.CheckField(validator.NotBlank(req.Email), "email", "this filed cannot be empty")
+	eval.CheckField(validator.NotBlank(req.Name), "name", "this field cannot be empty")
+	eval.CheckField(validator.NotBlank(req.Email), "email", "this field cannot be empty")
 	eval.CheckField(validator.Matches(req.Email, validator.EmailRX), "email", "must be a valid email")
 	eval.CheckField(validator.MinChars(req.Password, 8), "password", "password must be bigger than 8 chars")
 
