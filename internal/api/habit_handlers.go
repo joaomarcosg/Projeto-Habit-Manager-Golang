cbd1154e91f00a35281bdd82d04c81b75b692b07
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-07-07 16:24:29 -0300

    FEAT: adicionar handlerUpdateHabit nos handlers dos hábitos

--- internal/api/habit_handlers.go
+++ internal/api/habit_handlers.go
@@ -123,3 +123,56 @@ func handleDeleteHabit(svc *services.HabitService) http.HandlerFunc {
 
 	}
 }
+
+func handleUpdateHabit(svc *services.HabitService) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		userID, ok := r.Context().Value(userIDKey).(string)
+		if !ok || userID == "" {
+			utils.EncodeJson(w, r, http.StatusUnauthorized, map[string]any{
+				"error": "unauthorized",
+			})
+			return
+		}
+
+		data, problems, err := utils.DecodeValidJson[habit.CreateHabitReq](r)
+		if err != nil {
+			_ = utils.EncodeJson(w, r, http.StatusUnprocessableEntity, problems)
+			return
+		}
+
+		habit := entity.Habit{
+			Name:        data.Name,
+			Category:    data.Category,
+			Description: data.Description,
+			Frequency:   data.Frequency,
+			StartDate:   data.StartDate,
+			TargetDate:  data.TargetDate,
+			Priority:    data.Priority,
+		}
+
+		idStr := chi.URLParam(r, "id")
+		habitID, err := strconv.ParseInt(idStr, 10, 64)
+		if err != nil {
+			utils.EncodeJson(w, r, http.StatusBadRequest, map[string]any{
+				"error": "invalid param",
+			})
+			return
+		}
+
+		id, err := svc.UpdateHabit(r.Context(), userID, habitID, habit)
+		if err != nil {
+			slog.Error("failed to delete habit", "error", err)
+			utils.EncodeJson(w, r, http.StatusInternalServerError, map[string]any{
+				"error": "failed to update habit",
+			})
+			return
+		}
+
+		utils.EncodeJson(w, r, http.StatusOK, map[string]any{
+			"update habit ID": id,
+		})
+
+	}
+}
