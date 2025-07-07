b62bdc36c84da68bfeab6eaf91c1c7f4068f6f7c
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-07-07 15:54:03 -0300

    FEAT: adiciona método UpdateHabit

--- internal/services/habits_service.go
+++ internal/services/habits_service.go
@@ -26,3 +26,7 @@ func (s *HabitService) ListHabits(ctx context.Context, userID string) ([]entity.
 func (s *HabitService) DeleteHabit(ctx context.Context, userID string, id int64) (bool, error) {
 	return s.repo.DeleteHabit(ctx, userID, id)
 }
+
+func (s *HabitService) UpdateHabit(ctx context.Context, userID string, habit entity.Habit) error {
+	return s.repo.UpdateHabit(ctx, userID, habit)
+}
