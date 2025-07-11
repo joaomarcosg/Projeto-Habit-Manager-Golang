be07355eb1db5cc760e9bfa8e32dd47ab25f31df
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-07-11 11:31:03 -0300

    FEAT: adiciona queries para atualizar status do hábito (UpdateHabitStatus) e uma track  para acompanhar o progresso do hábito

---
+++ internal/store/mysqlstore/queries/habit_status.sql
@@ -0,0 +1,11 @@
+-- name: UpdateHabitStatus :exec
+INSERT INTO habit_status (habit_id, user_id, status, date)
+VALUES (?, ?, ?, ?)
+ON DUPLICATE KEY UPDATE
+    status = VALUES(status)
+    updated_at = CURRENT_TIMESTAMP;
+
+-- name: HabitTrack :one
+SELECT COUNT(*) FROM habit_status
+WHERE habit_id = ? AND user_id = ? AND status = 'done'
+AND date BETWEEN ? AND ?;
